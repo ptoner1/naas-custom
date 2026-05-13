cd8d3247c2e828375f09ed7888d57d8957872237
Paul Toner <ptoner92@gmail.com>
2026-05-13 14:55:37 -0400

    CORS Error fix for AWS container

--- naas-backend/src/main/java/com/example/demo/config/WebConfig.java
+++ naas-backend/src/main/java/com/example/demo/config/WebConfig.java
@@ -16,7 +16,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                // .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
@@ -28,7 +29,8 @@ public class WebConfig implements WebMvcConfigurer {
         CorsConfiguration config = new CorsConfiguration();
         
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:4200");
+        // config.addAllowedOrigin("http://localhost:4200");
+        config.addAllowedOrigin("*");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         
